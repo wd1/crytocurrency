53a7c5227617885af6b47fe7133541a350671a40
wd1 <akmksi211@gmail.com>
2017-12-07 21:50:31 -0500

    Update Email to Dashboard

---
+++ js/uicontrol.js
@@ -0,0 +1,10 @@
+function email_click() {
+    $(".main_subpanel").hide();
+    $("#mail_page").show();
+}
+
+function dashboard_click() {
+    console.log("SDF");
+    $(".main_subpanel").hide();
+    $("#dashboard_main").show();
+}
